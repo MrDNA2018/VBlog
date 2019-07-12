6dbb77305d539eeb49a6147295097b424c97498e
MrDNA2018 <toxiaoxiao20160703@gmail.com>
2019-07-12 14:10:53 +0800

    1、修改端口

--- vueblog/config/index.js
+++ vueblog/config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/': {
-        target: 'http://localhost:8081',
+        target: 'http://localhost:1111',
         changeOrigin: true,
         pathRewrite: {
           '^/': ''
@@ -22,7 +22,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 2222, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
